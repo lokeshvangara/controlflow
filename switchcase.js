2295b949543fc55d16b0f01eaf57a8994b542de1
lokeshvangara <99350727+lokeshvangara@users.noreply.github.com>
2022-03-07 14:51:13 +0530

    Create switchcase.js

---
+++ switchcase.js
@@ -0,0 +1,16 @@
+/** switch case */
+
+function job(name){
+    switch(name){
+        case 1:"professor"
+        console.log("you teach well");
+        break;
+        case 2:"artist"
+        console.log("i like painting");
+        break;
+        default:
+            console.log("what you do");
+    }
+}
+
+job("professor");   
