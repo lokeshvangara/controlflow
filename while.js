5f45597e7fa7e6edce2034a0aea1d48263421491
lokeshvangara <99350727+lokeshvangara@users.noreply.github.com>
2022-03-07 14:58:05 +0530

    Create while.js

---
+++ while.js
@@ -0,0 +1,23 @@
+/** while */
+
+ let i = 1;
+while(i <= 5 ) {
+    console.log(i);
+    i++;
+} 
+
+/** do while  */
+
+ let i = 1;
+do{
+    console.log(i);
+    i++;
+}while (i <= 5); 
+
+/** infinte loop */
+
+let i = 0
+
+while(i < 10){
+    console.log(i);
+} 
