58edadca10ef85d5a80b93cdfaebc8a6b3d96c93
lokeshvangara <99350727+lokeshvangara@users.noreply.github.com>
2022-03-07 14:49:31 +0530

    Create for statement.js

---
+++ for statement.js
@@ -0,0 +1,10 @@
+/** If statement & else statement */
+
+var name1=('lokesh')
+    name2=('lokesh')
+if(name1 == name2){
+    console.log("print lokesh")
+}
+else if (name1 !== name2){
+    console.log("errorProblem")
+};
