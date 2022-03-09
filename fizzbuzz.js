6e92bf7db7f294503cbeb282d6a9d03203932133
Lokesh <99350727+lokeshvangara@users.noreply.github.com>
2022-03-09 15:45:29 +0530

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,11 @@
+// divisible by 3 => Fizz
+const output = fizzbuzz(3);
+console.log(output); // expected out put is fizz
+
+//divisble by 5 => buzz
+const output = fizzbuzz(5);
+console.log(output); // expected output is buzz
+
+//divisble by both 3 & 5 => fizzbuzz
+const output = fizzbuzz(15);
+console.log(output); // expected output is fizzbuzz
